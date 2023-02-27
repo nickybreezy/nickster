ba9f48342fcd3e2309c773a34b257aa4c5652e12
Nicky <i493382@student.fontys.nl>
2023-02-27 15:03:24 +0100

    pushed login page and search albums page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container, InputGroup, FormControl, Button, Row, Card} from 'react-bootstrap';
+import {useState, useEffect} from "react";
+import Home from './Home'
+import Login from "./Login";
+import SearchAlbum from "./SearchAlbum";
+import {Route, Link, Router, BrowserRouter, Routes} from "react-router-dom";
+import NavBar from "./NavBar";
+
+const CLIENT_ID = "99e8f40ff31e4773afd9025afb9d63c2";
+const CLIENT_SECRET = "a0c007ed7ada4e0aa5eabfeb02a6ffc9";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    return (
+        <div className="App">
+
+            <BrowserRouter>
+                <NavBar/>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route exact path="/login" element={<Login/>}/>
+                    <Route exact path="/searchalbum" element={<SearchAlbum/>}/>
+                </Routes>
+            </BrowserRouter>
+
+
+        </div>
+    );
 }
 
 export default App;
